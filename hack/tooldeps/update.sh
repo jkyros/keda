openshift/kedacore-keda

UPSTREAM: <carry>: add tooldeps to generate go.mod for build tools
This is used by CPaaS where Cachito will make a cached copy of anything
needed during the build so that an offline brew build can succeed.

hack/tooldeps/update.sh
@@ -1,0 +1,10 @@
+branch=$(git branch --show-current)
+url="https://raw.githubusercontent.com/openshift/kedacore-keda/${branch}/Makefile"
+
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+
+rm -f go.mod go.sums
+go mod init tmp
+for i in $(sed -n '/call go-get-tool/ s/^.*,\(.*\))/\1/p' ../../Makefile ); do
+	go get -d $i
+done

jkyros/keda

hack/tooldeps/update.sh
@@ -1,0 +1,10 @@
+branch=$(git branch --show-current)
+url="https://raw.githubusercontent.com/openshift/kedacore-keda/${branch}/Makefile"
+
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+
+rm -f go.mod go.sum
+go mod init tmp
+for i in $(sed -n '/call go-get-tool/ s/^.*,\(.*\))/\1/p' ../../Makefile ); do
+	go get -d $i
+done